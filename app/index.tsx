38351703d0875fb68e7e5756aa0f24c10a059f12
AlvinQ <ltmervin123@gmail.com>
2026-06-22 22:11:45 +0800

    feats: Implement mobile app that embed the deployed web version of this prototype

---
+++ app/index.tsx
@@ -0,0 +1,28 @@
+import { StyleSheet } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { WebView } from 'react-native-webview';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+  },
+  webview: {
+    flex: 1,
+  },
+});
+
+export default function Index() {
+  const VERCEL_URL = 'https://web-app-3d-model-integration-demo.vercel.app/';
+  
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <WebView
+          source={{ uri: VERCEL_URL }}
+          style={styles.webview}
+        />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
